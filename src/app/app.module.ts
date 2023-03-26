3fbd1b9f786234f1b713839ab03201c319e572bd
Fernando Duarte Villavicencio <al222010329@gmail.com>
2023-03-26 17:35:00 -0600

    Navegacion para el sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { VinculacionComponent } from './vinculacion/vinculacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    VinculacionComponent
   ],
   imports: [
     BrowserModule,
